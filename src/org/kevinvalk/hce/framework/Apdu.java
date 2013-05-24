5face15203e78536e6b29413df7778d62fc4ef71
Kevin Valk <kevin@kevinvalk.nl>
2013-05-24 22:02:46 +0200

    LC read, iso exception

--- src/org/kevinvalk/hce/framework/Apdu.java
+++ src/org/kevinvalk/hce/framework/Apdu.java
@@ -1,5 +1,8 @@
 package org.kevinvalk.hce.framework;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
 import org.kevinvalk.hce.framework.apdu.HeaderApdu;
 
 import struct.JavaStruct;
@@ -39,7 +42,37 @@ public class Apdu
 		
 		
 	}
-
+	
+	public int getLength()
+	{
+		return getLength(1);
+	}
+	
+	/**
+	 * Returns the LC parameter from the apdu 
+	 * 
+	 * The LC field can be 1, 2 or 3 bytes large so use argument for that
+	 * 
+	 * @param size
+	 * @return
+	 */
+	public int getLength(int size)
+	{
+		int length = 0;
+		int l = (getBuffer().length < Iso7816.OFFSET_LC + size) ? (Iso7816.OFFSET_LC + size) - getBuffer().length : size;
+		for (int i = 0; i < l; i++)
+		{
+			length <<= 8; // Shift left
+			length |= getBuffer()[Iso7816.OFFSET_LC+i] & 0xFF; // Grab one byte and OR it
+		}
+		return length;
+	}
+	
+	public byte[] getData()
+	{
+		return Arrays.copyOfRange(getBuffer(), Iso7816.OFFSET_CDATA, getBuffer().length);
+	}
+	
 	public byte[] getBuffer()
 	{
 		return buffer;
